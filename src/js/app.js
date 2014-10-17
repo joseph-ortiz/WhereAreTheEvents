55c79a4390057ce25ae01d14d7059d82fed03e95
Joseph Ortiz <josepho011235@gmail.com>
2014-10-17 01:06:25 -0400

    added todo notes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,20 @@
 $(document).ready(function() {
 
-
+  //TODO: get user location
+  //TODO: center map to user location
+  //TODO: get search criteria
+  //TODO: query meetupAPI
+  //TODO: get coordinates
+  //TODO: create list
+  //TODO: map location on google Maps
 
   $.when(google.maps.event.addDomListener(window, 'load', initialize))
     .then(function(data, textStatus, jqXHR) {
       //alert(jqXHR.status); // Alerts 200
+
+
+
+
       $("#searchButton").click(function() {
         //TODO:Get inputs
         console.log("SearchButton Clicked! ");
