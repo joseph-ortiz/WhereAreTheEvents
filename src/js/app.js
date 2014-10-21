883ec261d6c2b2c8da9f95e8056c0c6a10c8e9b4
Joseph Ortiz <josepho011235@gmail.com>
2014-10-21 00:06:38 -0400

    meetup results now populate into the div result container

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   //TODO:Implement Require.js
   //TODO: get user location
   //TODO: center map to user location
-  //TODO: get search criteria
-  //TODO: query meetupAPI
-  //TODO: get coordinates
   //TODO: create list
   //TODO: map location on google Maps
   //TODO: reduce the size of the google maps image. Unnecessarily large. googlemaps opttion?
@@ -13,26 +10,35 @@ $(document).ready(function() {
   $.when(google.maps.event.addDomListener(window, 'load', initialize))
     .then(function(data, textStatus, jqXHR) {
       //alert(jqXHR.status); // Alerts 200
-
-
-
-
       $("#searchButton").click(function() {
         //TODO:Get inputs
         console.log("SearchButton Clicked! ");
         var place = $("#place").val();
-        var hobby = $("#hobby").val();
-
-        //TODO:search Get meetings;
-        var meetupResults = getMeeting(place, hobby);
-        //TODO: Add item to List
-
+        var hobby = $("#hobby").val(); //TODO: get search criteria + validation
+        var meetupResults = getMeeting(place, hobby); //TODO:search Get meetings;//TODO: query meetupAPI
         meetupResults.then(function(data) {
           console.log("meetup result complete");
-          //var _m = meetups[0];
+          _.each(data.results, function(item) {
 
-          $("ul.results").append("<h1>WhatIsthisATest</h1>")
+            var meetupEvent = { //TODO: get coordinates
+              name: item.name,
+              url: item.event_url,
+              status: item.status,
+              //description: item.description
+            };
+            var start = "<li><div>";
+            var end = "</div></li>";
+            var body = "";
+            _.forIn(meetupEvent, function(prop, key) {
+              var propText = '<p>' + key + ": " + prop + '</p>';
+              body += propText;
+            });
+            start += body;
+            start += end;
+            $("ul.results").append(start); //TODO: Add item to List
             //TODO: Add item on the Map
+          });
+
         });
 
       });
